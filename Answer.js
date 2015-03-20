7c90c8974aeebf41f5b16ec795a95b1b5b2dc8f6
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-20 00:25:28 +0000

    Update Answer.js

--- Answer.js
+++ Answer.js
@@ -1,6 +1,6 @@
 var $ = function (selector) {
-	var elements = [];
-	var findElements = new FindElements().parser(elements, selector);
+  var elements = [];
+  var findElements = new FindElements().parser(elements, selector);
   return elements;
 };
 
@@ -9,59 +9,59 @@ var FindElements = function() {
 };
 
 FindElements.prototype.parser = function(elements, selector) {
-	if(selector[0].match(/^[\.#]/)) { 
-		this._filterIDOrClass(elements, selector);
-	} else { 	
-		this._sortElementsByNumberOfSelectors(elements, selector.split(/(?=\.|#[A-z0-9]+)/g));
-	}
+  if(selector[0].match(/^[\.#]/)) { 
+    this._filterIDOrClass(elements, selector);
+  } else { 	
+    this._sortElementsByNumberOfSelectors(elements, selector.split(/(?=\.|#[A-z0-9]+)/g));
+  }
 };
 
 FindElements.prototype._filterIDOrClass = function(elements, selector) {
-	if(selector[0] == '#') { 
-		this._findElementById(elements, selector);
-	} else { 
-		this._findElementByClass(elements, selector);
-	}
+  if(selector[0] == '#') { 
+    this._findElementById(elements, selector);
+  } else { 
+    this._findElementByClass(elements, selector);
+  }
 };
 
 FindElements.prototype._findElementById = function(elements, id) {
-	this._addElements(elements, [document.getElementById(id.slice(1))]);
+  this._addElements(elements, [document.getElementById(id.slice(1))]);
 };
 
 FindElements.prototype._findElementByClass = function(elements, selectorClass) { 
-	var items = document.body.children;
-	for(var i = 0; i < items.length; i++) { 
-		if(items[i].className.match(selectorClass.slice(1))) { 
-			this._addElements(elements, [items[i]]);
-		}
-	}
+  var items = document.body.children;
+  for(var i = 0; i < items.length; i++) { 
+    if(items[i].className.match(selectorClass.slice(1))) { 
+      this._addElements(elements, [items[i]]);
+    }
+  }
 };
 
 FindElements.prototype._sortElementsByNumberOfSelectors = function(elements, selectors) { 
-	if(selectors.length === 1) { 
-		var foundElements = this._findElementByTag(selectors);
-		this._addElements(elements, foundElements);
-	} else { 
-		this._findElementsWithClassOrId(elements, selectors);
-	}
+  if(selectors.length === 1) { 
+    var foundElements = this._findElementByTag(selectors);
+    this._addElements(elements, foundElements);
+  } else { 
+    this._findElementsWithClassOrId(elements, selectors);
+  }
 };
 
 FindElements.prototype._findElementByTag = function(selector) {
-	return document.getElementsByTagName(selector);
+  return document.getElementsByTagName(selector);
 };
 
 FindElements.prototype._findElementsWithClassOrId = function(elements, selectors) { 
-	var tag = this._findElementByTag(selectors[0]);
-	for(var i = 0; i < tag.length; i++) { 
-		if(tag[i].id == selectors[i].slice(1) || tag[i].className.match(selectors[1].slice(1))) { 
-			this._addElements(elements, [tag[i]]);
-		}
-	}
+  var tag = this._findElementByTag(selectors[0]);
+  for(var i = 0; i < tag.length; i++) { 
+    if(tag[i].id == selectors[i].slice(1) || tag[i].className.match(selectors[1].slice(1))) { 
+      this._addElements(elements, [tag[i]]);
+    }
+  }
 };
 
 FindElements.prototype._addElements = function(elements, foundElements) {
-	var length = foundElements.length;
-	for(var i = 0; i < length; i++) { 
-		elements.push(foundElements[i]);
-	}
+  var length = foundElements.length;
+  for(var i = 0; i < length; i++) { 
+    elements.push(foundElements[i]);
+  }
 };
